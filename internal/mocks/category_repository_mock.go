4faf87b0ccdeb2bec550946f0bcaa20f70da87bf
chlovec <alfred.okoronkwo@gmail.com>
2025-06-22 13:50:53 -0700

    product repo interface

--- internal/mocks/category_repository_mock.go
+++ internal/mocks/category_repository_mock.go
@@ -7,44 +7,40 @@ import (
 	"product-services/internal/shared"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
 )
 
 type MockCategoryRepository struct {
-	GetCategoryByIDFunc func(ctx context.Context, id uuid.UUID) (*models.Category, error)
-	ListCategoriesFunc  func(ctx context.Context, opts shared.ListOptions) (*models.ListCategoriesResult, error)
-	CreateCategoryFunc  func(ctx context.Context, category *models.Category) error
-	UpdateCategoryFunc  func(ctx context.Context, category *models.Category) error
-	DeleteCategoryFunc  func(ctx context.Context, id uuid.UUID) error
+	mock.Mock
 }
 
 func (m *MockCategoryRepository) GetCategoryByID(
 	ctx context.Context,
 	id uuid.UUID,
 ) (*models.Category, error) {
-	return m.GetCategoryByIDFunc(ctx, id)
+	args := m.Called(ctx, id)
+	return args.Get(0).(*models.Category), args.Error(1)
 }
 
 func (m *MockCategoryRepository) ListCategories(
 	ctx context.Context,
 	opts shared.ListOptions,
 ) (*models.ListCategoriesResult, error) {
-	return m.ListCategoriesFunc(ctx, opts)
+	args := m.Called(ctx, opts)
+	return args.Get(0).(*models.ListCategoriesResult), args.Error(1)
 }
 
-func (m *MockCategoryRepository) CreateCategory(
-	ctx context.Context,
-	category *models.Category,
-) error {
-	return m.CreateCategoryFunc(ctx, category)
+func (m *MockCategoryRepository) CreateCategory(ctx context.Context, category *models.Category) error {
+	args := m.Called(ctx, category)
+	return args.Error(0)
 }
 
-func (m *MockCategoryRepository) UpdateCategory(
-	ctx context.Context,
-	category *models.Category,
-) error {
-	return m.UpdateCategoryFunc(ctx, category)
+func (m *MockCategoryRepository) UpdateCategory(ctx context.Context, category *models.Category) error {
+	args := m.Called(ctx, category)
+	return args.Error(0)
 }
 
 func (m *MockCategoryRepository) DeleteCategory(ctx context.Context, id uuid.UUID) error {
-	return m.DeleteCategoryFunc(ctx, id)
+	args := m.Called(ctx, id)
+	return args.Error(0)
 }
